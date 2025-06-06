74af841809d36fbed197d066821c67c8a6e02fbe
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-06-06 22:43:28 +0700

    add peptide project 6/6/2025

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,31 +4,42 @@ import bv from "../../assets/bv-logo.png";
 import xray from "../../assets/xray.jpg";
 import soundService from "../../assets/sound-service.jpg";
 import ProjectCard from "../../common/ProjectCard";
+import peptide from "../../assets/peptide.png";
 
 const Projects = () => {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
-      <h3 className="sectionTitle" style={{color: 'red'}}>Intern At 'Bệnh viện nhi đồng 2' 2 months. Perform small modules such as code debugging, code testing,
-      learning hospital procedures, and coded the pneumonia prediction application under the guidance of the company's seniors.</h3>
+      <h3 className="sectionTitle" style={{ color: "red" }}>
+        Intern At 'Bệnh viện nhi đồng 2' 2 months. Perform small modules such as
+        code debugging, code testing, learning hospital procedures, and coded
+        the pneumonia prediction application under the guidance of the company's
+        seniors.
+      </h3>
       <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={peptide}
+          link="https://github.com/hoofkhanh/transformer_xai_peptide"
+          h3="Antimicrobial Peptide Prediction (Transformer + XAI + Amazon EC2)"
+          p="AI"
+        />
         <ProjectCard
           src={xray}
           link="https://github.com/hoofkhanh/pneumonia_prediction-vision_transformer"
           h3="Pneumonia Prediction Using Vision Transformer Model"
-          p="Web App (AI)"
+          p="Web (AI)"
         />
         <ProjectCard
           src={soundService}
           link="https://github.com/hoofkhanh/springboot-microservice-sound_service_web"
           h3="Sound Service Web (Microservice)"
-          p="Web App"
+          p="Web"
         />
         <ProjectCard
           src={spotifyImage}
           link="https://github.com/hoofkhanh/springboot-music_web-root"
           h3="Spotify Clone"
-          p="Web App"
+          p="Web"
         />
       </div>
     </section>
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -2,30 +2,30 @@ import styles from "./SkillsStyles.module.css";
 import checkMarkLight from "../../assets/checkmark-light.svg";
 import checkMarkDark from "../../assets/checkmark-dark.svg";
 import SkillList from "../../common/SkillList";
-import {ThemeContext} from "../../common/ThemeContext";
+import { ThemeContext } from "../../common/ThemeContext";
 import { useContext } from "react";
 
 const Skills = () => {
-  const {theme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
-  const checkMarkIcon = theme === 'light'? checkMarkLight : checkMarkDark;
+  const checkMarkIcon = theme === "light" ? checkMarkLight : checkMarkDark;
 
-  return ( 
+  return (
     <section id="skills" className={styles.container}>
       <h1>Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="JAVA" />
-        <SkillList src={checkMarkIcon} skill="PYTHON (AI)" />
-        <SkillList src={checkMarkIcon} skill="Tensorflow" />
         <SkillList src={checkMarkIcon} skill="Spring boot" />
         <SkillList src={checkMarkIcon} skill="Spring cloud" />
-        <SkillList src={checkMarkIcon} skill="JS" />
         <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="Tensorflow" />
+        <SkillList src={checkMarkIcon} skill="Explainable AI (XAI)" />
+        <SkillList src={checkMarkIcon} skill="JS" />
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="Boostrap" />
       </div>
-      <hr  />
+      <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Postgresql" />
         <SkillList src={checkMarkIcon} skill="MySql" />
@@ -36,9 +36,10 @@ const Skills = () => {
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Git (basic)" />
         <SkillList src={checkMarkIcon} skill="Docker (basic)" />
+        <SkillList src={checkMarkIcon} skill="Amazon Web Services (EC2)" />
       </div>
     </section>
-   );
-}
- 
+  );
+};
+
 export default Skills;
